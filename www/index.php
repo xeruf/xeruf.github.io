2a0627d73254c5a27aa390af1d093c70dcdc96e2
Xerus <27jf@web.de>
2018-11-12 08:34:32 +0100

    Add filler content

--- www/index.php
+++ www/index.php
@@ -1,16 +1,30 @@
 <html>
 <head>
-    <title>Xerus2000</title>
+	<title>Xerus2000</title>
 
-    <meta charset="ISO-8859-1"/>
-    <meta name="author" content="Janek Fischer"/>
-    <meta name="keywords" content="Programming"/>
+	<meta charset="ISO-8859-1"/>
+	<meta name="author" content="Janek Fischer"/>
+	<meta name="keywords" content="Programming"/>
 
-    <link rel="shortcut icon" href="assets/iconred.png" type="image/x-icon" />
-    <link href="css/style.css" type="text/css" rel="stylesheet"/>
-    <link href="css/menu.css" type="text/css" rel="stylesheet"/>
+	<link rel="shortcut icon" href="assets/iconred.png" type="image/x-icon"/>
+	<link href="css/style.css" type="text/css" rel="stylesheet"/>
+	<link href="css/menu.css" type="text/css" rel="stylesheet"/>
 </head>
+
 <body>
-    <div id="head container"></div>
+<div class="container head">
+	<nav>
+		<ul>
+			<li><a href="#home">Home</a></li>
+			<li><a href="#projects">Projects</a></li>
+		</ul>
+	</nav>
+</div>
+<div class="container content">
+	<div id="home">Hey, I am Janek</div>
+	<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+	<div id="projects">These are my projects</div>
+</div>
+
 </body>
 </html>
