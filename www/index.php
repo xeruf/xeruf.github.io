c152b9ffa1418c07c0a905f976bc98c151050752
Xerus <27jf@web.de>
2018-11-12 08:30:15 +0100

    Add stuff

---
+++ www/index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>Xerus2000</title>
+
+    <meta charset="ISO-8859-1"/>
+    <meta name="author" content="Janek Fischer"/>
+    <meta name="keywords" content="Programming"/>
+
+    <link rel="shortcut icon" href="assets/iconred.png" type="image/x-icon" />
+    <link href="css/style.css" type="text/css" rel="stylesheet"/>
+    <link href="css/menu.css" type="text/css" rel="stylesheet"/>
+</head>
+<body>
+    <div id="head container"></div>
+</body>
+</html>
